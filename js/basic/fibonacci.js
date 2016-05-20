e7163916e6fe82dc2043332b2cabf5987a6f90c8
Brian Kemper <bkemper@gmail.com>
2016-05-20 14:51:25 -0600

    more solutions

--- js/basic/fibonacci.js
+++ js/basic/fibonacci.js
@@ -1,22 +1,27 @@
+///////////////////
+//nFib recursive//
+/////////////////
 function nFib(len, n, res) {
-  var temp = n;
   if (!arguments[2]) {
     res = [0,1];
   }
   if (len === res.length) {
     return res;
   }
-  if (n > res.length) {
-    temp = res.length;
-  }
   var pushArr = 0;
-  for (var i = res.length - 1; i > res.length - temp - 1; i--) {
-    pushArr += res[i];
+  for (var i = res.length - 1; i >= res.length - n; i--) {
+    if (res[i]) {
+      pushArr += res[i];
+    }
   }
   res.push(pushArr);
   return nFib(len, n, res);
 }
 
+
+//////////////////
+//fib recursive//
+////////////////
 function fib(len, res) {
   if (!arguments[1]) {
     res = [0,1];
@@ -26,4 +31,22 @@ function fib(len, res) {
   }
   res.push(res[res.length-1] + res[res.length-2])
   return fib(len, res);
+}
+
+
+///////////////////
+//nFib for loops//
+/////////////////
+function nFib(len, n) {
+  var ret = [0,1];
+  for (var i = 2; i < len; i++) {
+    var num = 0;
+    for (var j = ret.length - 1; j >= ret.length - n; j--) {
+      if (ret[j]) {
+        num += ret[j];
+      }
+    }
+    ret.push(num);
+  }
+  return ret;
 }
